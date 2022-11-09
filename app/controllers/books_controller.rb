b3d35ee45c4a32a12d69489b1d642e12d82eae43
mthskzk <116724950+mthskzk@users.noreply.github.com>
2022-11-09 09:50:41 +0000

    [Add]edit page,flash masage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,10 +17,23 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     book.save
-    # 4. index
-    redirect_to '/books/:id'
+    # 4. フラッシュメッセージを定義し、詳細画面へリダイレクト
+    flash[:notice] = "Book was successfully created"
+    redirect_to book_path(book.id)
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully update"
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
   private
   #ストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit'
-  post 'books' => 'books#create'
+  resources :books
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
