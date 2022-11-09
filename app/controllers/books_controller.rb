4b0ff61490435e453c5a2e9eb480374a6edef056
mthskzk <116724950+mthskzk@users.noreply.github.com>
2022-11-09 08:03:34 +0000

    [Add]index page,show page,

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,34 @@
 class BooksController < ApplicationController
+
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def create
+    # 1,2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. index
+    redirect_to '/books/:id'
+  end
+
+
+  private
+  #ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'books' => 'books#index'
   get 'books/:id' => 'books#show'
   get 'books/:id/edit' => 'books#edit'
+  post 'books' => 'books#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
