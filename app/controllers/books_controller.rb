fbdb0976a19db13bbf2f98cc257018bc9469fe3c
mthskzk <116724950+mthskzk@users.noreply.github.com>
2022-11-10 04:18:34 +0000

    [Edit]cssファイル

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -14,24 +15,38 @@ class BooksController < ApplicationController
 
   def create
     # 1,2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
+    # データがすべて入力されていたら
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
+    if @book.save
     # 4. フラッシュメッセージを定義し、詳細画面へリダイレクト
-    flash[:notice] = "Book was successfully created"
-    redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    # データが入力されていなければ
+    else
+      @books = Book.all
+      # render indexに行く前にindexにあるbooks変数に値をセットする
+      render :index
+    end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully update"
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    # データが入力され、アップデートされているかどうか
+    # データ入力されている
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully update."
+      redirect_to book_path(@book.id)
+    # データ入力されていない
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+     flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
@@ -41,7 +56,4 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :body)
   end
 
-
 end
-
-
